6fb70f55624e8de50775d407ce0d3516fb8523fa
basiliols <Larissabasilio1084@gmail.com>
2025-10-03 10:48:21 -0300

    .

--- Modulo2-JavaScript/Aula16-Consumindo API/buscaCEP.js
+++ Modulo2-JavaScript/Aula16-Consumindo API/buscaCEP.js
@@ -1,21 +1,58 @@
 //https://viacep.com.br/ws/colocar CEP/json
 
-const cep = document.getElementById("cep")
+const cep = document.getElementById("cep");
 
-cep.addEventListener("change" , (evento) => {
-    let cepUsuario = evento.target
-    buscaCEP(cepUsuario.valeu)
-})
+cep.addEventListener("change", (evento) => {
+  let cepUsuario = evento.target.value;
+  buscarCEP(cepUsuario);
+});
 
-async function buscarCEP(cepUsuario){
+async function buscarCEP(cepUsuario) {
+  let erroCep = document.getElementById("erro");
+  erroCep.innerHTML = ""; // Limpa erro anterior
 
-    try {
-        let consultaCEP = await fetch('https://viacep.com.br/ws/${cepUsuario}CEP/json')
-        let consultaCEPJson = await consultaCEP.json()
+  try {
+    let consultaCEP = await fetch(`https://viacep.com.br/ws/${cepUsuario}/json`);
+    let consultaCEPJson = await consultaCEP.json();
+
+    if (consultaCEPJson.erro) {
+      throw new Error("CEP INEXISTENTE");
     }
-    catch {
 
-    }
+    preencheCampos(consultaCEPJson);
 
-
+  } catch (error) {
+    erroCep.innerHTML = "CEP INVÁLIDO, TENTE NOVAMENTE !!";
+    apagarCampos()
+  }
 }
+
+function preencheCampos(cepJson) {
+  let rua = document.getElementById("rua");
+  let bairro = document.getElementById("bairro");
+  let cidade = document.getElementById("cidade");
+  let estado = document.getElementById("estado");
+
+  rua.value = cepJson.lougradora
+  bairro.value = cepJson.bairro
+  cidade.value = cepJson.localidade
+  estado.value = cepJson.uf
+}
+
+function apagarCampos(cepJson) {
+    let rua = document.getElementById("rua");
+    let bairro = document.getElementById("bairro");
+    let cidade = document.getElementById("cidade");
+    let estado = document.getElementById("estado");
+  
+    rua.value = ""
+    bairro.value = ""
+    cidade.value = ""
+    estado.value = ""
+  }
+  
+
+
+
+
+
