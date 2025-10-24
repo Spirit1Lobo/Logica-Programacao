6b303cf14c0eb2032d33505e77c392cc089dd6a3
Spirit1Lobo <spirit.play.games007@gmail.com>
2025-10-24 10:47:42 -0300

    .

---
+++ Modulo03-PHP/Aula19-IntroduçãoPHP/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Introdução PHP</title>
+</head>
+<body>
+
+    <h1>
+        <?php
+            echo "Hello World";
+        ?>
+    </h1>
+
+    <h2>Variaveis em PHP</h2>
+
+    <p>
+        <?php
+
+        $nome = "Diego";
+        $sobrenome = "Nakashima";
+
+        echo "Nome $nome <br>";
+        echo "Sobrenome $sobrenome <br>";
+        ?>
+    </p>
+
+    <h2>Constantes em PHP</h2>
+
+    <p>
+        <?php
+
+        const faculdade = "UMC";
+        const cidade = "Mogi das Cruzes";
+
+        echo "Faculdade" . faculdade . "<br>";
+        echo "Cidade" . cidade . "<br>";
+
+
+        ?>
+    </p>
+    
+</body>
+</html>
