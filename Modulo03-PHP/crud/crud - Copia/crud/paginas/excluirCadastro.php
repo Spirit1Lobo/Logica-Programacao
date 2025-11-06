a1a8be03ac1840b8c7e964a90a99ba8ae0e6b179
Spirit1Lobo <spirit.play.games007@gmail.com>
2025-11-06 10:55:47 -0300

    .

---
+++ Modulo03-PHP/crud/crud - Copia/crud/paginas/excluirCadastro.php
@@ -0,0 +1,36 @@
+<?php
+//valida se o $_POST["id] está vazio
+if(isset($_POST["id"])){
+
+    //conexão com o banco de dados
+    include("../conexao/conexao.php");
+
+    //criar variavel do ID
+    $id = $_POST["id"];
+
+    //prepara a consulta SQL para excluir cadastro
+    $sql = "DELETE FROM usuarios WHERE ID = ?";
+    $stmt = $conn->prepare($sql);
+
+    if ($stmt) {
+        $stmt->bind_param("i" , $id);
+        //Executa a query
+        $stmt->execute();
+        //direciona vocepara a pagina especificada 
+        header("location: verificarUsuario.php");
+        //encerra a consulta
+        $stmt-> close(); 
+    }else {
+        //mensagem de erro
+    echo "<div class='mensagem erro'>Erro na consulta</div>";
+    }
+    //encerra a conexão com o banco de dadoss
+     
+
+    
+
+
+
+}
+
+?>
