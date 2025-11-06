9601981b05affa361c73170178d0ba7657a49cba
Spirit1Lobo <spirit.play.games007@gmail.com>
2025-11-06 10:57:29 -0300

    .

---
+++ crud/crud/paginas/atualizarNota.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Atualizar Nota</title>
+    <link rel="stylesheet" href="../estilos/styleVerificar.css">
+</head>
+<body>
+    
+<header>
+        <nav>
+            <ul>
+                <li><a href="../index.html">Home</a></li>
+                <li><a href="#">Cadastrar Usuário</a></li>
+                <li><a href="verificarUsuario.php">Procurar Usuário</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <main>
+
+    <section>
+        <form action="atualizarNota.php" method="post">
+            <select name="curso" id="curso" class="estilo">
+                <option value="ads">Análise e Desenvolvimento de Sistema</option>
+                <option value="es">Engenharia de Software</option>
+                <option value="si">Sistema da informação</option>
+                <option value="cc">Ciencia da Computação</option>
+            </select>
+            <input type="submit" value="Buscar">
+        </form>
+    </section>
+
+</section>
+
+    <?php
+    //verificar se o $_post["curso] está preenchido
+    if(isset($_post["curso"])){
+
+        //Conexão com o Banco de Dados
+        include("../conexao/conexao.php");
+        $curso = $_POST["curso"];
+
+        //Preparando a consulta SQL
+        $sql = "SELECT * FROM usuarios WHERE curso = ?";
+        $stmt = $conn->prepare($ql);
+
+        if ($stmt) {
+
+            $stmt->bind_param("s", $curso);
+            $stmt->execute();
+            $resultado = $stmt->get_result();
+
+
+
+        }
+
+
+    }
+    ?>
+    </main>
+
+</body>
+</html>
